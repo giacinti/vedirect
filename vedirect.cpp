5b9cb54252fdfd07edcef7e324e693c0775438b1
Philippe Giacinti <philippe@giacinti.com>
2021-02-03 08:47:49 +0100

    first commit

---
+++ vedirect.cpp
@@ -0,0 +1,90 @@
+/* 
+   esphome vedirect component 
+   Copyright (C) 2021 philippe@giacinti.com
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include <stdarg.h>
+#include "vedirect.h"
+
+static const char *TAG = "vedirect";
+
+
+// VEDirectComponent::VEDirectComponent(UARTComponent* parent, int nbs, ...)
+//   : UARTDevice(parent) {
+//   int i=0;
+//   nb_sensors=0;
+//   va_list slist;
+//   va_start(slist,nbs);
+//   while (i<nbs) {
+//     VESensor* s=new VESensor;
+//     ve_sensors[nb_sensors++]=s;
+//     s->_label=strdup(va_arg(slist,const char*));
+//     s->_sensor=va_arg(slist,Sensor*);
+//     s->_is_text=(typeid(s->_sensor)==typeid(TextSensor));
+//     i+=2;
+//     ESP_LOGD(TAG,"recorded %s sensor for label %s", s->is_text()?"text":"numeric", s->label());
+//   }
+//   va_end(slist);
+// }
+
+void VEDirectComponent::_addSensor(const char* label, Sensor* ss, bool is_text) {
+  VESensor* s=new VESensor;
+  s->_label=strdup(label);
+  s->_sensor=ss;
+  s->_is_text=is_text;
+  ve_sensors[nb_sensors++]=s;
+  ESP_LOGD(TAG,"recorded %s sensor for label %s", s->is_text()?"text":"numeric", s->label());
+}
+
+void VEDirectComponent::addSensor(const char* label, TextSensor* ss) {
+  this->_addSensor(label,(Sensor*)ss,true);
+}
+
+void VEDirectComponent::addSensor(const char* label, Sensor* ss) {
+  this->_addSensor(label,ss,false);
+}
+
+void VEDirectComponent::ReadVEData() {
+  while (available()) {
+    vedfh.rxData(read());
+  }
+  yield();
+}
+
+void VEDirectComponent::update() {
+  ReadVEData();
+  int skip[max_sensors]={-1};
+  for (int i=0; i<vedfh.veEnd; i++) {
+    for (int j=0; j<nb_sensors; j++) {
+      if (skip[j]==0 && strcmp(ve_sensors[j]->label(),vedfh.veName[i])==0) {
+	skip[j]=1;
+	VESensor* s=ve_sensors[j];
+	Sensor* ss=s->sensor();
+	if (s->is_text()) {
+	  TextSensor* ts=(TextSensor*)ss;
+	  ts->publish_state(vedfh.veValue[i]);
+	  ESP_LOGD(TAG,"published state %s for text sensor %s", vedfh.veValue[i], s->label());
+	}
+	else {
+	  float val=atof(vedfh.veValue[i]);
+	  ss->publish_state(val);
+	  ESP_LOGD(TAG,"published state %f for numeric sensor %s", val, s->label());
+	}
+	break;
+      }
+    }
+  }
+}
+
+
---
+++ vedirect.h
@@ -0,0 +1,67 @@
+/* 
+   esphome vedirect component 
+   Copyright (C) 2021 philippe@giacinti.com
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#ifndef __VEDIRECT_H
+#define __VEDIRECT_H
+
+#include "esphome.h"
+using namespace esphome;
+using namespace sensor;
+using namespace uart;
+using namespace text_sensor;
+#include "VeDirectFrameHandler.h"
+
+class VEDirectComponent : public PollingComponent, public UARTDevice {
+public:
+  //VEDirectComponent(UARTComponent* parent, int, ...);
+ VEDirectComponent(UARTComponent* parent) : UARTDevice(parent) {}
+
+  void addSensor(const char*, TextSensor*);
+  void addSensor(const char*, Sensor*);
+  void setup() override {}
+  void loop() override {}
+  void update() override;
+
+protected:
+
+  struct VESensor {
+    char* _label;
+    Sensor* _sensor;
+    bool _is_text;
+
+  VESensor() : _label(0), _sensor(0), _is_text(false) {}
+    ~VESensor();
+
+    inline const char* label() {return _label;}
+    inline Sensor* sensor() {return _sensor;}
+    inline const bool is_text() {return _is_text;}
+  };
+  
+  static const int max_sensors=60;
+  VESensor* ve_sensors[max_sensors] = {NULL};
+  unsigned int nb_sensors;
+  void _addSensor(const char*, Sensor*, bool);
+
+  VeDirectFrameHandler vedfh;
+  void ReadVEData();
+
+  unsigned int ve_PID=0;
+  char ve_SERIAL[11]; // format LLYYMMSSSSS
+  char ve_FW[6]; // firmware version, same for 16 and 24 bits
+
+};
+
+#endif
